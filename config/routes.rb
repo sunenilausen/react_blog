a446f710e7fc810bd9be6cbd4297a511df0598c8
Sune Nilausen <sune@cs2.dk>
2017-05-20 22:36:21 +0200

    New feature. Api versioning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  resources :articles
+  namespace :api do 
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :articles
+    end
+  end
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  # devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
