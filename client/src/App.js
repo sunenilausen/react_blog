b86da458c6377cc8413574d0d389f092fd8cb38e
Sune Nilausen <sune@cs2.dk>
2017-05-20 20:52:25 +0200

    New feature. Login design

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Login/>
+      </MuiThemeProvider>
     );
   }
 }
