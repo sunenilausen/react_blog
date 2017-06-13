7ef9cbf6eb215a24d6e3cff2e3c5828eee333507
Sune Nilausen <sune@cs2.dk>
2017-06-13 12:24:35 +0200

    New feature. more styling on articles

--- client/src/components/ArticleCard.js
+++ client/src/components/ArticleCard.js
@@ -5,17 +5,10 @@ import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
 import { Link } from 'react-router-dom';
 
-const styles = {
-  root: {
-    marginTop: '1rem',
-    width: '100%'
-  }
-}
-
 class ArticleCard extends Component {
   render() {
     return (
-      <Card style={styles.root}>
+      <Card className="fixed-size">
         <Link to={`/blog/${this.props.title}`}>
           <CardHeader title={this.props.title}/>
         </Link>
--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import ArticleCard from './ArticleCard';
 
-const styles = {
-  root: {
-    display: 'flex',
-    justifyContent: 'start',
-    alignItems: 'center',
-    flexDirection: 'column',
-    minHeight: '100vh',
-    padding: '1rem'
-  }
-}
-
 class Articles extends Component {
   
 
   render() {
       let articles = this.props.articles
       return articles
-      ? <div style={styles.root}>
+      ? <div className="horizontal-container" width="300px">
           {Object.keys(articles).map((key) => {
             return <ArticleCard title={articles[key].title} content={articles[key].content} id={key}/>
           })} 
